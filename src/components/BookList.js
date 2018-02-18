ba1b8e88cfc6e78a0e104f3ad7a73b0561c4dbd3
Mateusz Stomski <lehronn@gmail.com>
2018-02-18 19:00:47 +0100

    fix bookList component

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,26 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
 import style from './BookList.css';
 import {addBook, removeBook, editBook} from '../actions'
 import Book from '../containers/BookContainer';
 
+const mapStateToProps = state => (
+  { books: state.books }
+);
+
 class BookList extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { books: {}}
-  }
+    this.state = { books: []
+  };
+}
 
   render() {
     return (
       <div className={style.bookList}>
-        const mapStateToProps = state => (
-          { books: state.books }
-          <ul>
-            {books.map(({ title, author, year, genere }) =>
-              <li>{title}-{author}-{year}-{genere}</li>
-            )}
-          </ul>
-        );
+        <ul>
+          {books.map(({ title, author, year, genere }) =>
+            <li>{title}-{author}-{year}-{genere}</li>
+          )}
+        </ul>
       </div>
     );
   }
